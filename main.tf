7ca3a676c1599c561123ce0a0bd34f24167668d9
ElizaRayner <elizarayner@sasktel.net>
2024-03-14 16:47:13 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,32 @@
-provider "aws" {
-  region     = "us-east-1"
-    access_key = var.ACCESS_KEY_ID
-    secret_key = var.ACCESS_KEY_VALUE
-    token      = var.SESSION_TOKEN
+variable "region" {}
+variable "access_key" {}
+variable "secret_key" {}
+
+variable "assume_role_arn" {}
+# variable "assume_role_session_name" {}
+# variable "assume_role_external_id" {}
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
 }
-variable "ACCESS_KEY_ID" {}
-variable "ACCESS_KEY_VALUE" {}
-variable "SESSION_TOKEN" {}
+
+provider "aws" {
+  region     = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+  assume_role {
+    role_arn = var.assume_role_arn
+    # session_name = var.assume_role_session_name
+    # external_id  = var.assume_role_external_id
+  }
+}
+
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
 
 variable "prefix" {}
 
